57a1d95954d69d361e275249dafe0b5c8a5c3de7
Djordje <djoksonv@gmail.com>
2017-10-28 01:00:14 +0200

    Fixed: Data cannot log if there is data with same hours and minutes in file. This is in case of power losing. TODO: Serial communication with windows PC

--- test-all-in-one/test-all-in-one.ino
+++ test-all-in-one/test-all-in-one.ino
@@ -1,36 +1,43 @@
 //Implementirati:
-/*  Ako se u nekom slucaju Arduino iskljuci, i nakon toga ukljuci, a ne uspe ocitati SD kartica u Setup-u, while petlju koja traje sve dok se kartica ne ucita
- *  Napraviti posebnu datoteku gde se upisuju vec upisane minutaze, a pre svakog upisa trenutna minutaza poredi sa tom iz datoteke
+/*
+    Napraviti posebnu datoteku gde se upisuju vec upisane minutaze, a pre svakog upisa trenutna minutaza poredi sa tom iz datoteke
 */
-
 #include <SPI.h>
-#include <SD.h>
+#include <SdFat.h>
 #include <DS3231.h>
 
 //include serial while debug mode is ON
 #define DEBUG 1
-#define EVERY_X_MINUTES 1
+#define EVERY_X_MINUTES 6
 
-DS3231  rtc(SDA, SCL);
+//SD Card
+SdFat SD;
 File myFile;
+
+//RTC Clock
+DS3231  rtc(SDA, SCL);
 Time t;
+
+//Testing existence of minute string in case of power losing
+char str[16];    //Must hold one line as a field
+size_t n;       //Length of returned field with delimiter
 
 //This string has to be logged via serial port if something went wrong
 String warningString;
 String dateStr;
 String full_date_str;
+String hour_minute;
 char file_name[10] = "asds";
 uint8_t string_len;
 bool file_exists = false;
 
-//SD chip select
+//SD chip select pin
 const int chipSelect = 4;
 bool sd_begin = false;
 
 uint8_t last_min = 0;
 uint8_t last_day = 0;
-uint8_t pomDan = 1;
-uint8_t pomMes = 1;
+uint8_t day_min_val[2];
 
 //interrupt variables
 const byte interrupt_pin = 2;
@@ -38,6 +45,8 @@ volatile int num_cycles = 0;
 int result;
 int result_old;
 bool kp = false;
+
+bool first_power_on = true;
 
 void setup() {
   // put your setup code here, to run once:
@@ -57,38 +66,25 @@ void setup() {
   Serial.print("Initializing SD card...");
 #endif
   // see if the card is present and can be initialized:
- /*
-  if (!SD.begin(chipSelect))
-  {
-#ifdef DEBUG
-    Serial.println("Card failed, or not present");
-#endif
-    // don't do anything more:
-    return;
-  }
-*/
   do
   {
-      sd_begin = SD.begin(chipSelect);
-      if(sd_begin == false)
-      {
-        Serial.println("Card failed, or not present");
-        delay(1000);
-      }
-   }while(sd_begin == false);
+    sd_begin = SD.begin(chipSelect);
+    if (sd_begin == false)
+    {
+      Serial.println("Card failed, or not present");
+      delay(1000);
+    }
+  } while (sd_begin == false);
 #ifdef DEBUG
   Serial.println("card initialized.");
 #endif
-  /*POSLE OBRISATI*/
- // rtc.setDate(26, 10, 2017);
-  /**************************************/
 }
 
 void loop() {
 
   // put your main code here, to run repeatedly:
   t = rtc.getTime();
-  
+
   //If day is changed, then make a new log file
   if (t.date != last_day)
   {
@@ -96,13 +92,13 @@ void loop() {
     dateStr = dateStringPreparation(full_date_str);
 
     string_len = dateStr.length() + 1;
-    
+
     dateStr.toCharArray(file_name, string_len);
 
     file_exists = SD.exists(file_name);
     Serial.print("File exists: ");
     Serial.println(file_exists);
-    
+
     myFile = SD.open(file_name, FILE_WRITE);
     if (!myFile)
     {
@@ -117,7 +113,7 @@ void loop() {
       Serial.println("myFile OPEN OK");
 #endif
     }
-    if(file_exists == false)
+    if (file_exists == false)
     {
       myFile.println(full_date_str);
       myFile.println("Interval,Cycles");
@@ -126,52 +122,90 @@ void loop() {
 
     last_day = t.date;
   }
-  /*
-  pomDan++;
-  if (pomDan > 30)
-  {
-    pomDan = 1;
-    pomMes  ++;
-    if (pomMes > 12)
-      pomMes = 1;
-  }
-  */
 
-  //rtc.setDate(pomDan, pomMes, 2017);
+  //NAPRAVITI: Da se last_min cita iz neke datoteke na SD kartici, ili neko poredjenje i minuta i sata
   if ((t.min % EVERY_X_MINUTES == 0) && (t.min != last_min))
   {
     myFile = SD.open(file_name, FILE_WRITE);
+    //This condition is for case of power losing.
+    //Then, Arduino must first check logfile, and in case that there is not data
+    //logged with the same hours and minutes, log the newest data.
+    if (first_power_on == true)
+    {
+      first_power_on = false;
+      myFile.rewind();
+      while (myFile.available() > 0)
+      {
+        n = readField(&myFile, str, sizeof(str), "\n");
+        if (n == 0)
+        {
+          Serial.println("To few lines");
+        }
+        else
+        {
+          Serial.print("Size of str: ");
+          Serial.println(n);
+          Serial.print("Str: ");
+          Serial.println(str);
+        }
+      }
+      char *p   = str;
+      uint8_t i = 0;
+      while (*p)
+      {
+        if (isdigit(*p) && i < 2)
+        {
+          day_min_val[i] = strtol(p, &p, 10);
+          Serial.print("dayMinVal[");
+          Serial.print(i);
+          Serial.print("]: ");
+          Serial.println(day_min_val[i]);
+          i++;
+        }
+        else
+          p++;
+      }
+    }
+    if (t.hour == day_min_val[0] && t.min == day_min_val[1])
+    {
+      Serial.println("Sati i minuti su jednaki!");
+      delay(500);
+    }
+    else
+    {
+      cli();    //disable interrupt
 
-    cli();    //disable interrupt
+      hour_minute = String(t.hour, DEC);
 
-    myFile.print(String(t.hour, DEC));
-    myFile.print(":");
-    myFile.print(String(t.min, DEC));
-    myFile.print(",");
-    myFile.println(String(num_cycles, DEC));
+      myFile.print(hour_minute);
+      myFile.print(":");
+      myFile.print(String(t.min, DEC));
+      myFile.print(",");
+      myFile.println(String(num_cycles, DEC));
 
-    Serial.println("Inserted number of cycles: ");
-    Serial.print(String(t.hour, DEC));
-    Serial.print(":");
-    Serial.print(String(t.min, DEC));
-    Serial.print(" - ");
-    Serial.println(String(num_cycles, DEC));
+      Serial.println("Inserted number of cycles: ");
+      Serial.print(String(t.hour, DEC));
+      Serial.print(":");
+      Serial.print(String(t.min, DEC));
+      Serial.print(" - ");
+      Serial.println(String(num_cycles, DEC));
 
-    num_cycles = 0;
-    sei();   //enable interrupt
-    last_min = t.min;
-    myFile.close();
-  }
-  cli();
-  result_old = result;
-  result = num_cycles;
-  sei();
-  if (result_old != result)
+      num_cycles = 0;
+      sei();   //enable interrupt
+      last_min = t.min;
+      myFile.close();
+    }
+    cli();
+    result_old = result;
+    result = num_cycles;
+    sei();
+    if (result_old != result)
     {
       Serial.print("Number of cycles: ");
       Serial.println(num_cycles);
     }
-  // delay(2000);
+  }
+
 }
 
 //Interrupt function
@@ -190,7 +224,6 @@ void cycle_increment()
   {
     num_cycles++;
     kp = false;
-    //last_interrupt_time = interrupt_time;
   }
 }
 
@@ -212,3 +245,22 @@ String dateStringPreparation(String str)
 
   return str;
 }
+
+//Function for reading one filed from CSV file
+size_t readField(File* file, char* str, size_t size, const char* delim) {
+  char ch;
+  size_t n = 0;
+  while ((n + 1) < size && file->read(&ch, 1) == 1) {
+    // Delete CR.
+    if (ch == '\r') {
+      continue;
+    }
+    str[n++] = ch;
+    if (strchr(delim, ch)) {
+      break;
+    }
+  }
+  str[n] = '\0';
+  return n;
+}
+
